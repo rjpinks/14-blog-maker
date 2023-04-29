b954a5248ec856115599ad57078cd2128a5f8d0c
Ryan <pinkstonr54@gmail.com>
2023-04-29 18:52:13 -0500

    added a couple of blank css files but started the frontend

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+//required packages
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const session = require('express-session');
 
+//setting up port & express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//calls handlebars and sequelize connects to the db
 const hbs = exphbs.create({});
-
 const sequelize = require("./config/connection");
 
 app.engine("handlebars", hbs.engine)
 app.set("view engine", "handelbars");
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('./controllers'));
 
+//starts the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
