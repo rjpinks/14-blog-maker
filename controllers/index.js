db300ad72f82958dcd617d3a25ccfdf3a667d2a2
Ryan <pinkstonr54@gmail.com>
2023-06-29 11:22:43 -0500

    trying to get this to work

--- controllers/index.js
+++ controllers/index.js
@@ -1,33 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const { blogs } = require('../models')
+const renderRoutes = require('./render-routes');
 
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
-
-router.get("/", async (req, res) => {
-  blogs.findAll()
-    .then(results => {
-      const blog = results.map(result => result.get({ plain: true }))
-      res.render('home', {
-        username: '',
-        blogs: data})
-    })
-});
-
-router.get("/dashboard", async (req, res) => {
-  res.render('dashboard');
-});
-
-router.get('/signup', async (req, res) => {
-  res.render('signup');
-});
-
-router.get('login', async (req, res) => {
-  res.render('login');
-});
+router.use('/', renderRoutes);
 
 module.exports = router;
---
+++ controllers/render-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+
+router.get("/", async (req, res) => {
+    Blog.findAll()
+      .then(results => {
+        const blog = results.map(result => result.get({ plain: true }))
+        res.render('home', {
+          username: '',
+          blogs: data})
+      })
+  });
+  
+router.get("/dashboard", async (req, res) => {
+    res.render('dashboard');
+  });
+  
+router.get('/signup', async (req, res) => {
+    res.render('signup');
+  });
+  
+router.get('login', async (req, res) => {
+    res.render('login');
+  });
+
+module.exports = router
