d6e5c427ffd79453f784ef5a04a51afbd6f71799
Ryan <pinkstonr54@gmail.com>
2023-06-28 20:07:35 -0500

    reworking front-end & changed server.js to get ready for sessions

--- server.js
+++ server.js
@@ -8,9 +8,28 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 //calls handlebars and sequelize connects to the db
 const hbs = exphbs.create({});
-const sequelize = require("./config/connection");
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine)
 app.set("view engine", "handelbars");
@@ -19,7 +38,7 @@ app.set("view engine", "handelbars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(require('./controllers'));
+app.use(require('./controllers'));
 
 //starts the server
 sequelize.sync({ force: false }).then(() => {
