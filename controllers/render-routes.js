17c748516dfd46c67191fcc38b6d3bf9a44b6f26
Ryan <pinkstonr54@gmail.com>
2023-06-29 19:02:20 -0500

    another small mistake

--- controllers/render-routes.js
+++ controllers/render-routes.js
@@ -20,7 +20,7 @@ router.get('/signup', async (req, res) => {
     res.render('signup');
   });
   
-router.get('login', async (req, res) => {
+router.get('/login', async (req, res) => {
     res.render('login');
   });
 
