6fa18c9cf2b406e5a420ee8933ac6c3c06bab767
Ryan <pinkstonr54@gmail.com>
2023-07-01 18:37:11 -0500

    once again another test on dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,17 +1,24 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#blog-form').value.trim();
+    const name = document.querySelector('#blog-title').value.trim();
     const content = document.querySelector('#blog-content').value.trim();
   
     if (name && content) {
       const response = await fetch(`/api/blogs`, {
         method: 'POST',
-        body: JSON.stringify({ name, content }),
         headers: {
           'Content-Type': 'application/json',
         },
-      });
+        body: JSON.stringify({ 
+          post_title: name.value, 
+          content: content.value
+        }),
+      })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data);
+      })
   
       if (response.ok) {
         document.location.replace('/');
