b1aecf8a4c55716b8d38236cf0588fd11b91f13a
Ryan <pinkstonr54@gmail.com>
2023-07-01 19:45:33 -0500

    still trying to get dashboard to work the way i want

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -23,7 +23,7 @@ const newFormHandler = async (event) => {
       })
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.reload();
       } else {
         alert('Failed to create blog');
       }
