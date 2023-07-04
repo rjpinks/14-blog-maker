37ff802d39c33a70337e676e2a4519757c760984
Ryan <pinkstonr54@gmail.com>
2023-07-03 20:50:06 -0500

    added login and logout routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -37,12 +37,54 @@ router.get('/:id', async (req, res) => {
       const newUser = await User.create({
         username: req.body.username,
         password: req.body.password,
-        email: req.body.email
       });
       res.status(200).json(newUser);
     } catch (err) {
       res.status(400).json(err);
     }
+});
+
+//logging in and logging out
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
