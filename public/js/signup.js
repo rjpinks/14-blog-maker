6165d239f10e8c438bd3e1fb6eee2c56fd0810eb
Ryan <pinkstonr54@gmail.com>
2023-07-03 20:02:50 -0500

    sign up test

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,13 +4,16 @@ const signupFormHandler = async (event) => {
     // const name = document.querySelector('#name-signup').value.trim();
     const username = document.querySelector('#user-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+
+    console.log('username -> ', username);
+    console.log('password -> ', password);
   
     if (username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({
-          username: username.value,
-          password: password.value
+          username: username,
+          password: password
         }),
         headers: { 'Content-Type': 'application/json' },
       });
